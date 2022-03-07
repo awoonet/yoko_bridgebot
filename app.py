5dbe79171efa493de2f1acf273ba7b18d97346ca
akatsukinoyami <akatsukinoyami@yahoo.com>
2022-03-08 00:48:39 +0200

    fix problem with async

--- app.py
+++ app.py
@@ -1,35 +1,61 @@
 from os import getenv as env
+import asyncio, logging, traceback
 from dotenv import load_dotenv
 
 from telegram.kclient import Telegram as app
 from discord.kclient import Discord as disc
 from classes.psql import Database as psql
-import classes, asyncio, logging, traceback
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
+db = psql()
+dc = disc()
+tg = app("session/yoko", env("API_ID"), env("API_HASH"), bot_token=env("TG_TOKEN"))
 
-async def main(tg, dc, db):
+
+async def main():
     try:
         await db.init_table()
-        await tg.start()
+
+        tg.db, dc.db = db, db
+        tg.dc, dc.tg = dc, tg
+
+        await tg.kstart()
         await dc.start(env("DC_TOKEN"))
-    except KeyboardInterrupt:
-        await dc.logout()
-        await tg.stop()
-        await db.close()
+
     except Exception as e:
         tg.send_error(e, traceback.format_exc())
         logging.exception("Exception occurred")
 
 
-if __name__ == "__main__":
+try:
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
 
-    db = psql()
-    dc = disc()
-    tg = app()
+except KeyboardInterrupt:
+    loop.run_until_complete(tg.stop())
+    loop.run_until_complete(dc.logout())
+    loop.run_until_complete(db.close())
+    loop.stop()
 
-    tg.db, dc.db = db, db
-    tg.dc, dc.tg = dc, tg
+finally:
+    loop.close()
 
-    asyncio.run(main(tg, dc, db))
+
+# if __name__ == "__main__":
+#     # tg.run(main(tg, dc, db))
+
+
+#     tg.db, dc.db = db, db
+#     tg.dc, dc.tg = dc, tg
+#     loop = asyncio.new_event_loop()
+#     # task = asyncio.gather(db.init_table(), dc.start(env("DC_TOKEN")), tg.start())
+#     # loop.run_until_complete(task)
+
+#     loop.create_task(dc.start(env("DC_TOKEN")))
+#     loop.create_task(tg.kstart())
+#     loop.create_task(db.init_table())
+#     loop.run_forever()
