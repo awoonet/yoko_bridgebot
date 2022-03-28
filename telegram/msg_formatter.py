076c03ca01243e96654c204905ed46c5db931cfb
dankild <dan.kild@speedandfunction.com>
2022-03-28 11:30:51 +0300

    fix error

--- telegram/msg_formatter.py
+++ telegram/msg_formatter.py
@@ -4,7 +4,6 @@ import re
 
 
 async def fetch_text(msg: Message) -> str:
-    """Возвращает caption, text или ничего из сообщения"""
     if msg.text is not None:
         return msg.text.markdown.replace("__", "_")
     elif msg.caption is not None:
@@ -14,30 +13,32 @@ async def fetch_text(msg: Message) -> str:
 
 
 async def fetch_name(msg: Message) -> str:
-    """
-    Возвращает username, имя с фамилией или просто имя
-    отформатированное в соответствии с полученной командой.
-    """
     if msg.from_user is not None:
         user = msg.from_user
-        if user.last_name is not None:
-            return f"{user.first_name} {user.last_name}"
-        else:
-            return user.first_name
+    elif msg.forward_from is not None:
+        user = msg.forward_from
     elif msg.sender_chat is not None:
         return msg.sender_chat.title
 
+    if user.last_name is not None:
+        return f"{user.first_name} {user.last_name}"
+    else:
+        return user.first_name
+
 
 async def forwarded(msg: Message) -> str:
+    if msg.forward_from_chat is not None:
+        title = msg.forward_from_chat.title
+        return f"\n**Переслано из канала: {title}**\n"
+
     if msg.forward_from is not None:
-        return (
-            f"\n**Переслано от пользователя: {await fetch_name(msg.forward_from)}**\n"
-        )
+        name = await fetch_name(msg)
     elif msg.forward_sender_name is not None:
-        return f"\n**Переслано от пользователя: {msg.forward_sender_name}**\n"
-    elif msg.forward_from_chat is not None:
-        return f"\n**Переслано из канала: {msg.forward_from_chat.title}**\n"
-    return ""
+        name = msg.forward_sender_name
+    else:
+        return ""
+
+    return f"\n**Переслано от пользователя: {name}**\n"
 
 
 async def format_msg(msg: Message) -> str:
@@ -48,31 +49,41 @@ async def format_msg(msg: Message) -> str:
 
 
 async def reply_msg(msg: Message) -> str:
-    if msg.reply_to_message is not None:
+    reply_not_none = msg.reply_to_message is not None
+
+    if reply_not_none:
         text = await format_msg(msg.reply_to_message)
         return f"В ответ на сообщение:\n{text}\n\n"
-    return ""
+    return reply_not_none
+
+
+async def guess_for_embed(msg: Message) -> tuple:
+    to_embed = ""
+
+    msg_text = await format_msg(msg)
+    if re.search(r"\[.+\]\(.+\)", msg_text):
+        to_embed = msg_text
+        content = None
+    else:
+        content = msg_text
+
+    reply = await reply_msg(msg)
+    if reply:
+        to_embed += reply
+
+    return (content, to_embed)
+
+
+async def make_embed_discord(text: str) -> Embed | None:
+    embed = None
+    if text:
+        embed = Embed()
+        embed.description = text
+    return embed
 
 
 async def text_formatter(self, msg: Message) -> tuple:
-    rpl = await reply_msg(msg)
-    msg = await format_msg(msg)
-
-    content, to_embed = "", ""
-
-    if re.search(r"\[.+\]\(.+\)", msg):
-        to_embed += msg
-        content = None
-    else:
-        content += msg
-
-    if rpl:
-        to_embed += rpl
-
-    if to_embed:
-        embed = Embed()
-        embed.description = to_embed
-    else:
-        embed = None
+    content, to_embed = await guess_for_embed(msg)
+    embed = await make_embed_discord(to_embed)
 
     return (content, embed)
